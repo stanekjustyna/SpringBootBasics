670727e3ecddf5baece5c06137f44c29c5d72435
stanekjustyna <justynka.stanek.js@gmail.com>
2019-12-17 19:31:05 +0100

    create simple controller method returning XML data

--- src/main/java/js/tasks/SpringBootBasics/task2/Task02Controller.java
+++ src/main/java/js/tasks/SpringBootBasics/task2/Task02Controller.java
@@ -1,13 +1,21 @@
 package js.tasks.SpringBootBasics.task2;
 
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Task02Controller {
 
-    @GetMapping("/task02")
+    @GetMapping(value="/task02")
     public Animal getAnimal(){
         return new Animal(1, "Reksio", 3);
     }
+
+    @GetMapping(value="/task02xml", produces= MimeTypeUtils.APPLICATION_XML_VALUE)
+    public Animal getAnimal2(){
+        return new Animal(2, "Burek", 3);
+    }
+
+
 }
