952af05479f07ce2675f2bc4149ddb0cfb486873
stanekjustyna <justynka.stanek.js@gmail.com>
2019-12-23 16:44:32 +0100

    add methods to controller

--- src/main/java/js/tasks/SpringBootBasics/task5/Task05Controller.java
+++ src/main/java/js/tasks/SpringBootBasics/task5/Task05Controller.java
@@ -1,16 +1,21 @@
 package js.tasks.SpringBootBasics.task5;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class Task05Controller {
 
     private AnimalRepository animalRepository;
+    private static Logger LOG = LoggerFactory.getLogger(Task05Controller.class);
 
     @Autowired
     public Task05Controller(AnimalRepository animalRepository) {
@@ -26,5 +31,36 @@ public class Task05Controller {
     @GetMapping(name = "/getAllAnimals", produces = MimeTypeUtils.APPLICATION_JSON_VALUE)
     public List<Animal> getAllAnimals(){
         return animalRepository.findAll();
+    }
+
+    @DeleteMapping("/deleteAnimal/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable long id){
+        if(animalRepository.findById(id).isPresent()){
+            animalRepository.deleteById(id);
+            return ResponseEntity.accepted().build();
+        }
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/getAnimalById/{id}")
+    public ResponseEntity<Animal> getAnimalById(@PathVariable long id){
+        LOG.info("Searching for Animal id= {}", id);
+
+        Optional<Animal> animal = animalRepository.findById(id);
+
+        if(animal.isPresent()){
+            LOG.info("Found animal: {}", animal.get());
+            return ResponseEntity.ok(animal.get());
+        }else{
+            LOG.info("No animal found");
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/saveAnimals")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveAnimals(@RequestBody List<Animal> animals){
+        animalRepository.saveAll(animals);
     }
 }
